86f7878dafeb9dc9925279818722aa091c3d9bde
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2024-08-29 04:38:28 +0530

    Testing Component

--- InvoiceBillingNativeTS/App.tsx
+++ InvoiceBillingNativeTS/App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import "./global.css";
+import TestButton from "./components/TestButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl">
-        Open up App.tsx to start working on your app!
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-xl mb-4">
+        Open up App.tsx to start working oyour app!
       </Text>
+      <TestButton
+        title="Test Button"
+        onPress={() => console.log("Button pressed!")}
+      />
       <StatusBar style="auto" />
     </View>
   );
